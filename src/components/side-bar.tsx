0a407b4a608b4201be43e34e8863f25c08f7e80b
yosephhabtamu <yosephhabtamu5@gmail.com>
2025-07-19 03:39:14 +0300

    Update logo text from 'Reveshare' to 'Dashboard' in side-bar component

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -95,7 +95,7 @@ export const Logo = () => {
         animate={{ opacity: 1 }}
         className="font-medium whitespace-pre text-black dark:text-white"
       >
-        Reveshare
+        Dashboard
       </motion.span>
     </a>
   );
